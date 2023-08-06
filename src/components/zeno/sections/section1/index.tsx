252eadf4b24234f3b7022ca07f41de9343584ed7
DUPROS Leegan <leegan.dupros@epitech.eu>
2023-08-06 19:40:24 +0200

    [~] minor fixes

--- src/components/zeno/sections/section1/index.tsx
+++ src/components/zeno/sections/section1/index.tsx
@@ -6,7 +6,7 @@ import Award from '../../components/Award'
 
 export default function Section1() {
   return (
-    <div className="flex flex-col w-full text-black px-4 md:px-14 mb-[90px]">
+    <div className="flex flex-col w-full text-black px-4 md:px-14 mb-20">
       <button className="bg-yellow-700/10 border border-yellow-700 text-yellow-700 rounded-full py-2 px-6 text-sm md:text-xl self-center md:self-start">
         Marvelous Ceramic Works
       </button>
@@ -32,14 +32,23 @@ export default function Section1() {
       </div>
 
       <div className="grid md:grid-cols-4 gap-4 h-[379px]">
-        <div className="bg-gray-500 rounded-xl md:col-span-3">
+        <div className="bg-gray-500 rounded-xl md:col-span-3 bg-[url('/img/hero-pattern.svg')]">
+          {/* <Image
+            className="  bg-gray-500"
+            src="/.svg"
+            alt="Next.js Logo"
+            width={72}
+            height={25}
+            priority
+          /> */}
           <div className="absolute">
-            <p className="font-light text-4xl m-6">
-              See what we Zeno can do for you
+            <p className="text-white font-light text-lg md:text-4xl m-4 md:m-6">
+              See what we Zeno
+              <br />
+              can do for you
               <span>
                 <svg
-                  width="32"
-                  height="32"
+                  className="w-6 h-6 md:w-8 md:h-8 "
                   viewBox="0 0 32 32"
                   fill="none"
                   xmlns="http://www.w3.org/2000/svg"
@@ -54,14 +63,32 @@ export default function Section1() {
           </div>
         </div>
         <div className=" bg-gray-500 rounded-xl">
-          <Image
+          {/* <Image
             className="  bg-gray-500"
             src="/.svg"
             alt="Next.js Logo"
             width={72}
             height={25}
             priority
-          />
+          /> */}
+          <div className="absolute">
+            <p className="text-white font-light text-lg md:text-4xl m-4 md:m-6">
+              Our Gallery
+              <span>
+                <svg
+                  className="w-6 h-6 md:w-8 md:h-8 "
+                  viewBox="0 0 32 32"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M12.395 7.60506C12.395 8.35173 12.995 8.9384 13.7283 8.9384H21.1817L6.66167 23.4584C6.14167 23.9784 6.14167 24.8184 6.66167 25.3384C7.18167 25.8584 8.02167 25.8584 8.54167 25.3384L23.0617 10.8184V18.2717C23.0617 19.0051 23.6617 19.6051 24.395 19.6051C25.1283 19.6051 25.7283 19.0051 25.7283 18.2717V7.60506C25.7283 6.87173 25.1283 6.27173 24.395 6.27173H13.7283C12.995 6.27173 12.395 6.87173 12.395 7.60506Z"
+                    fill="white"
+                  />
+                </svg>
+              </span>
+            </p>
+          </div>
         </div>
       </div>
     </div>
