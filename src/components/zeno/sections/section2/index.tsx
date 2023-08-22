f4e1c8e5c0d07c069be2bf7b3f8a6cc26a78cccd
DUPROS Leegan <leegan.dupros@epitech.eu>
2023-08-22 22:23:05 +0200

    [+] minor fixes

--- src/components/zeno/sections/section2/index.tsx
+++ src/components/zeno/sections/section2/index.tsx
@@ -1,33 +1,54 @@
+import { useMediaQuery } from '@/components/zeno/hooks/useMediaQuery'
 import Image from 'next/image'
 import React from 'react'
 
 export default function Section2() {
   return (
-    <div className="flex-col w-full bg-yellow-700 py-[90px]">
-      <div className="flex flex-row bg-red-100 mt-[24px] justify-between ">
-        <button className="text-xl bg-red-200 border border-red-800 rounded-full py-[8px] px-[24px]">
-          About us
+    <div className="md:grid grid-cols-3 gap-10 w-full bg-yellow-700 py-[40px] md:py-[90px] px-4 md:px-14">
+      <div>
+        <button
+          className="text-base text-white rounded-full py-[8px] px-[24px]"
+          style={{ background: 'rgba(255, 255, 255, 0.15)' }}
+        >
+          About Us
         </button>
-
-        <h3 className="md:text-[56px]">
-          Where passion and creativity come to life through the art of ceramics
-        </h3>
       </div>
 
-      <div className="md:flex justify-between pt-[112px]">
-        <Image
-          className="flex bg-black"
-          src="/.svg"
-          alt="Next.js Logo"
-          width={72}
-          height={25}
-          priority
-        />
-        <p>
-          Based in Sleman, we are a vibrant and inclusive community of artists,
-          enthusiasts, and learners, dedicated to exploring the endless
-          possibilities of clay.
-        </p>
+      <div className="md:col-span-2 justify-between">
+        <h3
+          className="text-5xl md:text-[56px] text-white"
+          style={{ fontWeight: 500, letterSpacing: -4.48 }}
+        >
+          Where passion and creativity come to life through the art of ceramics
+        </h3>
+
+        <div className="md:grid grid-flow-row grid-cols-2 mt-6 md:mt-10 ">
+          <div
+            className="w-full md:w-[408px] h-[372px] rounded-xl"
+            style={{ background: '#BD832F' }}
+          >
+            <Image
+              className="flex "
+              src="/.svg"
+              alt="Next.js Logo"
+              width={72}
+              height={25}
+              priority
+            />
+          </div>
+          <p
+            className="mt-4 md:mt-0 text-white text-base md:text-2xl"
+            style={{
+              fontWeight: '400',
+              lineHeight: 'normal',
+              letterSpacing: '-1.92px',
+            }}
+          >
+            Based in Sleman, we are a vibrant and inclusive community of
+            artists, enthusiasts, and learners, dedicated to exploring the
+            endless possibilities of clay.
+          </p>
+        </div>
       </div>
     </div>
   )
--- src/components/zeno/sections/section3/index.tsx
+++ src/components/zeno/sections/section3/index.tsx
@@ -18,16 +18,30 @@ const CarouselItem = () => {
 
 export default function Section3() {
   return (
-    <div className="flex-col w-full py-[90px]">
-      <div className="">
-        <button>Our Services</button>
-        <h1 className="text-yellow-700 text-5xl">Browse Categories</h1>
+    <div className="w-full py-[40px] md:py-[90px] px-4 md:px-14">
+      <div className="md:grid grid-cols-3 gap-10">
+        <div>
+          <button
+            className="text-yellow-700 text-xl rounded-full"
+            style={{
+              padding: '8px 24px',
+              background: 'rgba(161, 98, 7, 0.15)',
+            }}
+          >
+            Our Services
+          </button>
+        </div>
+        <div className="col-span-2">
+          <h1 className="text-yellow-700 text-5xl">Browse Categories</h1>
+        </div>
+      </div>
+
+      <div className="md:grid grid-cols-3 gap-10 mt-4">
         <p className=" text-neutral-600 text-2xl font-normal">
           Embark on a Journey of Inspiration: Explore the Rich Tapestry of
           Ceramic Craftsmanship
         </p>
-
-        <div className="grid grid-cols-4 gap-4 overflow-scroll">
+        <div className="col-span-2 grid grid-cols-3 gap-4 overflow-scroll">
           <CarouselItem />
           <CarouselItem />
           <CarouselItem />
